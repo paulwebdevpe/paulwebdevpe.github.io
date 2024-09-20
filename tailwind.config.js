bebbd919a6a9e205428f89bcf74f6fcbbf1d44d9
Paul <paul.cantoral07@gmail.com>
2024-09-20 17:44:46 +0200

    creato portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./portfolio/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
+  theme: {
+    extend: {
+      keyframes: {
+        rotate: {
+          "0%": { transform: "rotate(0deg)" },
+          "100%": { transform: "rotate(360deg)" },
+        },
+      },
+      animation: {
+        rotate: "rotate 10s linear infinite",
+      },
+    },
+  },
+  plugins: [require("flowbite/plugin"), require("tailwindcss-animated")],
+};
