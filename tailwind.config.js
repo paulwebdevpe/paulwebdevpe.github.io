5bc0202e6dac82579cc390d5d51574c27017602e
Paul <paul.cantoral07@gmail.com>
2024-09-20 18:30:55 +0200

    creato portfolioo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./portfolio/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
+  content: ["../portfolio/**/*.{html,js}","./index.html",  "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {
       keyframes: {
